3530c149270a1e7e46f14f0bc995e58d3cabe9da
Nithin <vustepallisaisrinithin@gmail.com>
2025-02-04 23:22:20 +0530

    modified:   lib/main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -77,228 +77,233 @@ class _FreightSearchFormState extends State<FreightSearchForm> {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(16.0),
-      child: Column(
-        children: [
-          Row(
+      child: Card(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
             children: [
-              Expanded(
-                child: Autocomplete<String>(
-                  optionsBuilder: (textEditingValue) async {
-                    return await _fetchSuggestions(textEditingValue.text);
-                  },
-                  onSelected: (selection) {
-                    _originController.text = selection;
-                  },
-                  fieldViewBuilder:
-                      (context, controller, focusNode, onEditingComplete) {
-                    return TextField(
-                      controller: controller,
-                      focusNode: focusNode,
-                      decoration: const InputDecoration(
-                        prefixIcon: ImageIcon(
-                          AssetImage('assets/location.png'),
-                          size: 10,
-                        ),
-                        labelText: 'Origin',
-                        border: OutlineInputBorder(),
-                      ),
-                    );
-                  },
-                ),
-              ),
-              const SizedBox(width: 10),
-              Expanded(
-                child: Autocomplete<String>(
-                  optionsBuilder: (textEditingValue) async {
-                    return await _fetchSuggestions(textEditingValue.text);
-                  },
-                  onSelected: (selection) {
-                    _destinationController.text = selection;
-                  },
-                  fieldViewBuilder:
-                      (context, controller, focusNode, onEditingComplete) {
-                    return TextField(
-                      controller: controller,
-                      focusNode: focusNode,
-                      decoration: const InputDecoration(
-                        prefixIcon: ImageIcon(
-                          AssetImage('assets/location.png'),
-                          size: 10,
-                        ),
-                        labelText: 'Destination',
-                        border: OutlineInputBorder(),
-                      ),
-                    );
-                  },
-                ),
-              ),
-            ],
-          ),
-          const SizedBox(height: 10),
-          Row(
-            children: [
-              Checkbox(
-                  value: isChecked,
-                  onChanged: (bool? value) {
-                    setState(() {
-                      isChecked = value!;
-                    });
-                  }),
-              const Text("Data"),
-            ],
-          ),
-          const SizedBox(height: 10),
-          Row(
-            children: [
-              Expanded(
-                child: DropdownButtonFormField<String>(
-                  value: selectedCommodity,
-                  decoration: const InputDecoration(
-                    labelText: 'Commodity',
-                    border: OutlineInputBorder(),
-                  ),
-                  items: commodities.map((String commodity) {
-                    return DropdownMenuItem<String>(
-                      value: commodity,
-                      child: Text(commodity),
-                    );
-                  }).toList(),
-                  onChanged: (newValue) {
-                    setState(() {
-                      selectedCommodity = newValue;
-                    });
-                  },
-                ),
-              ),
-              const SizedBox(width: 10),
-              Expanded(
-                child: TextField(
-                  controller: _dateController,
-                  readOnly: true,
-                  decoration: InputDecoration(
-                    labelText: 'Cut Off Date',
-                    border: const OutlineInputBorder(),
-                    suffixIcon: IconButton(
-                      icon: ImageIcon(
-                        AssetImage('assets/calendar.png'),
-                        size: 20,
-                      ),
-                      onPressed: () => _selectDate(context),
+              Row(
+                children: [
+                  Expanded(
+                    child: Autocomplete<String>(
+                      optionsBuilder: (textEditingValue) async {
+                        return await _fetchSuggestions(textEditingValue.text);
+                      },
+                      onSelected: (selection) {
+                        _originController.text = selection;
+                      },
+                      fieldViewBuilder:
+                          (context, controller, focusNode, onEditingComplete) {
+                        return TextField(
+                          controller: controller,
+                          focusNode: focusNode,
+                          decoration: const InputDecoration(
+                            prefixIcon: ImageIcon(
+                              AssetImage('assets/location.png'),
+                              size: 10,
+                            ),
+                            labelText: 'Origin',
+                            border: OutlineInputBorder(),
+                          ),
+                        );
+                      },
                     ),
                   ),
-                ),
-              ),
-            ],
-          ),
-          Text('heading'),
-          Row(
-            children: [
-              const Text("FCL"),
-              Checkbox(
-                  value: isChecked1,
-                  onChanged: (bool? value) {
-                    setState(() {
-                      isChecked1 = value!;
-                    });
-                  }),
-              const Text("LCL"),
-              Checkbox(
-                  value: isChecked2,
-                  onChanged: (bool? value) {
-                    setState(() {
-                      isChecked2 = value!;
-                    });
-                  }),
-            ],
-          ),
-          const SizedBox(height: 10),
-          Row(
-            children: [
-              Expanded(
-                child: DropdownButtonFormField<String>(
-                  decoration: const InputDecoration(
-                    labelText: 'Container Size',
-                    border: OutlineInputBorder(),
+                  const SizedBox(width: 10),
+                  Expanded(
+                    child: Autocomplete<String>(
+                      optionsBuilder: (textEditingValue) async {
+                        return await _fetchSuggestions(textEditingValue.text);
+                      },
+                      onSelected: (selection) {
+                        _destinationController.text = selection;
+                      },
+                      fieldViewBuilder:
+                          (context, controller, focusNode, onEditingComplete) {
+                        return TextField(
+                          controller: controller,
+                          focusNode: focusNode,
+                          decoration: const InputDecoration(
+                            prefixIcon: ImageIcon(
+                              AssetImage('assets/location.png'),
+                              size: 10,
+                            ),
+                            labelText: 'Destination',
+                            border: OutlineInputBorder(),
+                          ),
+                        );
+                      },
+                    ),
                   ),
-                  items: [
-                    '20\' Standard',
-                    '40\' Standard',
-                    '40\' High Cube',
-                    '45\' High Cube'
-                  ]
-                      .map((size) => DropdownMenuItem<String>(
-                            value: size,
-                            child: Text(size),
-                          ))
-                      .toList(),
-                  onChanged: (newValue) {
-                    setState(() {
-                      // Update selected container size (if needed)
-                    });
-                  },
-                ),
-              ),
-              const SizedBox(width: 10),
-              Expanded(
-                child: TextField(
-                  decoration: const InputDecoration(
-                    labelText: 'No of Boxes',
-                    border: OutlineInputBorder(),
-                  ),
-                  keyboardType: TextInputType.number,
-                ),
-              ),
-              const SizedBox(width: 10),
-              Expanded(
-                child: TextField(
-                  decoration: const InputDecoration(
-                    labelText: 'Weight (Kg)',
-                    border: OutlineInputBorder(),
-                  ),
-                  keyboardType: TextInputType.number,
-                ),
-              ),
-            ],
-          ),
-          const SizedBox(height: 10),
-          const Divider(),
-          const SizedBox(height: 10),
-          const Align(
-            alignment: Alignment.centerLeft,
-            child: Text(
-              "Container Internal Dimensions:",
-              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-            ),
-          ),
-          const SizedBox(height: 10),
-          Row(
-            children: [
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: const [
-                  Text("Length: 39.46 ft", style: TextStyle(fontSize: 14)),
-                  Text("Width: 7.70 ft", style: TextStyle(fontSize: 14)),
-                  Text("Height: 7.84 ft", style: TextStyle(fontSize: 14)),
                 ],
               ),
-              const SizedBox(width: 50),
-              Image.asset(
-                'assets/container.png',
-                width: 180,
-                height: 80,
+              const SizedBox(height: 10),
+              Row(
+                children: [
+                  Checkbox(
+                      value: isChecked,
+                      onChanged: (bool? value) {
+                        setState(() {
+                          isChecked = value!;
+                        });
+                      }),
+                  const Text("Data"),
+                ],
+              ),
+              const SizedBox(height: 10),
+              Row(
+                children: [
+                  Expanded(
+                    child: DropdownButtonFormField<String>(
+                      value: selectedCommodity,
+                      decoration: const InputDecoration(
+                        labelText: 'Commodity',
+                        border: OutlineInputBorder(),
+                      ),
+                      items: commodities.map((String commodity) {
+                        return DropdownMenuItem<String>(
+                          value: commodity,
+                          child: Text(commodity),
+                        );
+                      }).toList(),
+                      onChanged: (newValue) {
+                        setState(() {
+                          selectedCommodity = newValue;
+                        });
+                      },
+                    ),
+                  ),
+                  const SizedBox(width: 10),
+                  Expanded(
+                    child: TextField(
+                      controller: _dateController,
+                      readOnly: true,
+                      decoration: InputDecoration(
+                        labelText: 'Cut Off Date',
+                        border: const OutlineInputBorder(),
+                        suffixIcon: IconButton(
+                          icon: ImageIcon(
+                            AssetImage('assets/calendar.png'),
+                            size: 20,
+                          ),
+                          onPressed: () => _selectDate(context),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Text('heading'),
+              Row(
+                children: [
+                  const Text("FCL"),
+                  Checkbox(
+                      value: isChecked1,
+                      onChanged: (bool? value) {
+                        setState(() {
+                          isChecked1 = value!;
+                        });
+                      }),
+                  const Text("LCL"),
+                  Checkbox(
+                      value: isChecked2,
+                      onChanged: (bool? value) {
+                        setState(() {
+                          isChecked2 = value!;
+                        });
+                      }),
+                ],
+              ),
+              const SizedBox(height: 10),
+              Row(
+                children: [
+                  Expanded(
+                    child: DropdownButtonFormField<String>(
+                      decoration: const InputDecoration(
+                        labelText: 'Container Size',
+                        border: OutlineInputBorder(),
+                      ),
+                      items: [
+                        '20\' Standard',
+                        '40\' Standard',
+                        '40\' High Cube',
+                        '45\' High Cube'
+                      ]
+                          .map((size) => DropdownMenuItem<String>(
+                                value: size,
+                                child: Text(size),
+                              ))
+                          .toList(),
+                      onChanged: (newValue) {
+                        setState(() {
+                          // Update selected container size (if needed)
+                        });
+                      },
+                    ),
+                  ),
+                  const SizedBox(width: 10),
+                  Expanded(
+                    child: TextField(
+                      decoration: const InputDecoration(
+                        labelText: 'No of Boxes',
+                        border: OutlineInputBorder(),
+                      ),
+                      keyboardType: TextInputType.number,
+                    ),
+                  ),
+                  const SizedBox(width: 10),
+                  Expanded(
+                    child: TextField(
+                      decoration: const InputDecoration(
+                        labelText: 'Weight (Kg)',
+                        border: OutlineInputBorder(),
+                      ),
+                      keyboardType: TextInputType.number,
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 10),
+              const Divider(),
+              const SizedBox(height: 10),
+              const Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  "Container Internal Dimensions:",
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                ),
+              ),
+              const SizedBox(height: 10),
+              Row(
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: const [
+                      Text("Length: 39.46 ft", style: TextStyle(fontSize: 14)),
+                      Text("Width: 7.70 ft", style: TextStyle(fontSize: 14)),
+                      Text("Height: 7.84 ft", style: TextStyle(fontSize: 14)),
+                    ],
+                  ),
+                  const SizedBox(width: 50),
+                  Image.asset(
+                    'assets/container.png',
+                    width: 180,
+                    height: 80,
+                  ),
+                ],
+              ),
+              const SizedBox(height: 20),
+              OutlinedButton.icon(
+                onPressed: () {},
+                icon: ImageIcon(
+                  AssetImage('assets/search.png'),
+                  size: 20,
+                ),
+                label: Text("Search"),
               ),
             ],
           ),
-          const SizedBox(height: 20),
-          OutlinedButton.icon(
-            onPressed: () {},
-            icon: ImageIcon(
-              AssetImage('assets/search.png'),
-              size: 20,
-            ),
-            label: Text("Search"),
-          ),
-        ],
+        ),
       ),
     );
   }
